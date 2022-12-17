e2b0b4d2b7f3ddf31ffb8cb3afc548814d9d1145
Daniel-Loaiza <danielloaizaisi@gmail.com>
2022-12-17 11:56:59 -0500

    Created Node class

---
+++ DataStructures/LinkedLists/SinglyLinkedLists/Node.py
@@ -0,0 +1,30 @@
+#Node of a Singly Linked List
+class Node:
+
+    #constructor
+    def __init__(self):
+        self.data = None
+        self.next = None
+
+    #method for setting the data field of the node
+    def setData(self,data):
+        self.data = data
+
+    #method for getting the data field of the node
+    def getData(self):
+        return self.data
+
+    #method for setting the next field of the node
+    def setNext(self,next):
+        self.next = next
+
+    #method for getting the next field of the node
+    def getNext(self):
+        return self.next
+
+    #returns true if the node points to another node
+    def hasNext(self):
+        return self.next != None
+    
+    def __str__(self):
+        return f"Node with value of {self.data} pointing to {self.next}"
